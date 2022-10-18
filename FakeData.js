b16a5dd20e73a9b87df2deb80a1ac20c7217f91d
Deepanshu <deepanshu@rapidinnovation.dev>
2022-10-18 18:36:52 +0530

    Learning Graphql

---
+++ FakeData.js
@@ -0,0 +1,34 @@
+const UsersList = [
+  {
+    name: "John Doe",
+    username: "rock_1_star",
+    age: 23,
+    nationality: "American",
+  },
+  {
+    name: "John Wick",
+    username: "boogie_man",
+    age: 43,
+    nationality: "American",
+  },
+  {
+    name: "Bruce Banner",
+    username: "hulk",
+    age: 49,
+    nationality: "American",
+  },
+  {
+    name: "T-Challa",
+    username: "black_panther",
+    age: 35,
+    nationality: "Wankandan",
+  },
+  {
+    name: "Wanda Maximoff",
+    username: "scarlet_witch",
+    age: 26,
+    nationality: "Russian",
+  },
+];
+
+module.exports = { UsersList };
